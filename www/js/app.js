90a60a7d5f37fd5dc8adaefc2ce0cdb420deb70c
anujay kumar <anujay.kumar@msllearning.com>
2015-10-16 18:43:13 +0530

    template seperated

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,33 @@
 		
 var myApp=angular.module("myModule",['ionic','ui.router'])
 	
+	.config(["$stateProvider","$urlRouterProvider",function ($stateProvider,$urlRouterProvider) {
+			
+		$urlRouterProvider
+			.otherwise('oops');
+		$stateProvider
+		    .state('dashboard',{
+		        	url:"/dashboard",
+		        	templateUrl:'templates/dashboard.html',
+		        	controller:"myCtrl"
+		        })
+		    .state('courses',{
+		        	url:'/courses',
+		        	templateUrl:'templates/courses.html',
+		        	controller:'myCtrl'
+		        })
+		    .state('transactions',{
+		        	url:'/transactions',
+		        	templateUrl:'templates/transactions.html'
+		        })
+		    .state('oops',{
+		    	url:'/oops',
+		    	templateUrl:'templates/oops.html',
+		    	controller:'myCtrl'
+		    })
+		}])
+
+
 	.controller("myCtrl",["$scope","$ionicSideMenuDelegate",function($scope,$ionicSideMenuDelegate) {
 	 
 		$scope.toggleLeft = function() {
@@ -9,3 +36,5 @@ var myApp=angular.module("myModule",['ionic','ui.router'])
 		$scope.title="My App";
 		
 	}]);
+
+	
