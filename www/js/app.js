797ab09b5697336de761efba205d9f8f9b58139b
anujay kumar <anujay.kumar@msllearning.com>
2015-10-20 18:00:16 +0530

    router

--- www/js/app.js
+++ www/js/app.js
@@ -17,56 +17,62 @@ var myApp=angular.module("myModule",['ionic','ui.router','Login_Ctrl'])
 		        	controller:'loginCtrl'
 		        })
 		    .state('logout',{
-		        	url:"/logout",
-		        	templateUrl:'templates/logout.html',
-		        	controller:'baseCtrl'
-		        })
-		    .state('dashboard',{
-		        	url:"/dashboard",
+		    	templateUrl:'templates/logout.html',
+		        controller:'baseCtrl'
+		    })
+		   
+		    .state('menue',{
+		        	url:"/menue",
 		        	templateUrl:'templates/menue.html',
 		        	controller:'baseCtrl'
 		        })
-		    .state('courses',{
+		    .state('menue.courses',{
 		        	url:'/courses',
 		        	templateUrl:'templates/courses.html',
 		        	controller:'baseCtrl'
 		        })
-		    /*.state('result',{
+		    .state('menue.result',{
 		        	url:'/result',
 		        	templateUrl:'templates/results.html',
 		        	controller:'baseCtrl'
 		        })
-		    .state('transactions',{
+		    .state('menue.transactions',{
 		        	url:'/transactions',
 		        	templateUrl:'templates/transactions.html',
 		        	controller:'baseCtrl'
 		        })
-		    .state('cards',{
+		    .state('menue.cards',{
 		        	url:'/cards',
 		        	templateUrl:'templates/cards.html',
 		        	controller:'baseCtrl'
 		        }) 
-		    .state('contact',{
+		    .state('menue.contact',{
 		        	url:'/contact',
 		        	templateUrl:'templates/contact.html',
 		        	controller:'baseCtrl'
-		        })*/
-		    .state('oops',{
+		        })
+		    .state('menue.oops',{
 		    	url:'/oops',
 		    	templateUrl:'templates/oops.html',
 		        controller:'baseCtrl'
 		    })
 	    $urlRouterProvider
-			.otherwise('oops');
+			.otherwise('menue/oops');
 		}])
 
 
-	.controller("baseCtrl",["$scope","$ionicSideMenuDelegate",function($scope,$ionicSideMenuDelegate) {
+	.controller("baseCtrl",["$scope","$ionicSideMenuDelegate","$location",function($scope,$ionicSideMenuDelegate,$location) {
 	 
 		$scope.toggleLeft = function() {
 		    $ionicSideMenuDelegate.toggleLeft();
 		  };
 		$scope.title="My App";
+
+		$scope.logOut=function(){
+			 $location.path("logout");
+		}
+
+		
 		
 	}]);
 	
